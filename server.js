773cda81a0b45338675e94f2da8702ca2c32ca88
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-09-04 12:52:42 +0300

    create route admin/products upload images

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const authRouter = require("./routes/auth/auth-routes");
-// const adminProductsRouter = require("./routes/admin/products-routes");
+const adminProductsRouter = require("./routes/admin/products-routes");
 // const adminOrderRouter = require("./routes/admin/order-routes");
 
 // const shopProductsRouter = require("./routes/shop/products-routes");
@@ -46,7 +46,7 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
-// app.use("/api/admin/products", adminProductsRouter);
+app.use("/api/admin/products", adminProductsRouter);
 // app.use("/api/admin/orders", adminOrderRouter);
 
 // app.use("/api/shop/products", shopProductsRouter);

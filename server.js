d4909f045eeeed69c844a87b7c73b34516cad439
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-09-13 13:59:32 +0300

    Shopping view review functionality implementation

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const shopCartRouter = require("./routes/shop/cart-routes");
 const shopAddressRouter = require("./routes/shop/address-routes");
 const shopOrderRouter = require("./routes/shop/order-routes");
 const shopSearchRouter = require("./routes/shop/search-routes");
-// const shopReviewRouter = require("./routes/shop/review-routes");
+const shopReviewRouter = require("./routes/shop/review-routes");
 
 // const commonFeatureRouter = require("./routes/common/feature-routes");
 
@@ -55,7 +55,7 @@ app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", shopSearchRouter);
-// app.use("/api/shop/review", shopReviewRouter);
+app.use("/api/shop/review", shopReviewRouter);
 
 // app.use("/api/common/feature", commonFeatureRouter);
 
